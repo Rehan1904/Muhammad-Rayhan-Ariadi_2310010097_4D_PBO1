45c83c453cb8a84a1d180265bd1d97087ff7538e
Fukaziroh <rm26277@gmail.com>
2025-07-04 21:22:19 +0800

    Menambah Program Class Monster

--- src/Game/Monster.java
+++ src/Game/Monster.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Game;
 
-/**
- *
- * @author USER
- */
 public class Monster {
-    
+    public class Monster extends Karakter {
+    private String jenis;
+    private int expReward;
+
+    public Monster(String nama, String jenis, int health, int attackPower, int expReward) {
+        super(nama, health, attackPower);
+        this.jenis = jenis;
+        this.expReward = expReward;
+    }
+
+    // Method khusus monster
+    public String getJenis() {
+        return jenis;
+    }
+
+    public int getExpReward() {
+        return expReward;
+    }
+
+    // Override method serang untuk menambahkan efek khusus
+    @Override
+    public void serang(Karakter target) {
+        System.out.println(getNama() + " menyerang dengan cakar!");
+        super.serang(target);
+    }
 }
